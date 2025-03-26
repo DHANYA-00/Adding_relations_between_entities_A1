75831bad020d912eb43e7cd41f5db2261a1dbc1f
Dhanya Lakshmi s s <dhanyalakshmi.s.s.06@gmail.com>
2025-03-26 13:54:12 +0530

    completed

---
+++ src/component/DestinationCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../component/DestinationCard.css'
+
+function DestinationCard({name,location,image,description,price}) {
+  return (
+    <div className='main'>
+        <div className='sub'>
+            <h3>{name}</h3>
+            <h3>{location}</h3>
+            <img src={image} alt="Image" />
+            <p>{description}</p>
+            <h3>{price}</h3>
+
+        </div>
+    </div>
+  )
+}
+
+export default DestinationCard
